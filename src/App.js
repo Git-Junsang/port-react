a2615c1f0b4dc0325aab79228da6613088972c25
EC2 Default User <ec2-user@ip-172-31-43-109.ap-northeast-2.compute.internal>
2024-05-18 14:19:44 +0000

    Router Set

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
+// App.js
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import HomeView from "./views/HomeView";
 
 const App = () => {
     return (
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<HomeView />} />
-        </Routes>
-    </BrowserRouter>
+        <HomeView />
     );
 };
 
--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Header from "../components/Header";
-import Intro from "../components/Intro";
-import Skill from "../components/Skill";
-import Site from "../components/Site";
-import Port from "../components/Port";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import Skip from "../components/Skip";
-import Main from "../components/Main";
+import Header from "../components/Header.jsx";
+import Intro from "../components/Intro.jsx";
+import Skill from "../components/Skill.jsx";
+import Site from "../components/Site.jsx";
+import Port from "../components/Port.jsx";
+import Contact from "../components/Contact.jsx";
+import Footer from "../components/Footer.jsx";
+import Skip from "../components/Skip.jsx";
+import Main from "../components/Main.jsx";
 
 const HomeView = () => {
     return (
